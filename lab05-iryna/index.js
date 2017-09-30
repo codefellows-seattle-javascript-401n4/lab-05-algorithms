7a5553556da448b94ed8d8b82f171d5a271fabff
Iryna Maslova <maslovai@hotmail.com>
2017-09-30 15:53:54 -0700

    filter works

--- lab05-iryna/index.js
+++ lab05-iryna/index.js
@@ -15,11 +15,6 @@ console.log('index. pushed: ', listItem);
 listItem.pop();
 console.log('index. popped: ', listItem);
 
+console.log('index. for each: ', listItem.forEach(ele=>ele+' changed'));
 
-
-console.log('index. for each: ', listItem.forEach(ele=>ele + ' changed'));
-
-
-
-// listItem.filterList();
-// console.dir(listItem);
+console.log('index. filter: ', listItem.filterList(ele=>ele.length<=3));
--- lab05-iryna/lib/create-list.js
+++ lab05-iryna/lib/create-list.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 const list = module.exports = function(...args){
   for (let i = 0; i < args.length; i++){
     this[i] = args[i];
@@ -7,12 +8,14 @@ const list = module.exports = function(...args){
   this.length = args.length;
 };
 
+
 list.prototype.push = function(value){
   this[this.length] = value;
   this.length++;
  // console.log ('inside');
  return this;
 };
+
 
 list.prototype.pop = function(){
   let changed = this[this.length-1];
@@ -22,23 +25,26 @@ list.prototype.pop = function(){
   return changed;
 };
 
+
 list.prototype.forEach = function(fn) {
   let listCopy = Object.assign({}, this);
-  // console.log('newList inside createList', listCopy);
+
   for(let i = 0; i < listCopy.length; i++) {
     listCopy[i] = fn(listCopy[i]);
   }
-  // console.log('forEach. inside ', listCopy);
   return listCopy;
 }
 
+
 list.prototype.filterList = function(fn) {
-  let listCopy = Object.assign({}, this);
-  for(let i = 0; i < listCopy.length; i++) {
-    if (listCopy[i] >= 3){
-      delete(listCopy[i]);
-    }
-  };
-  console.log('filter. inside ', listCopy);
+
+     let listCopy = new list;
+     for(let i = 0; i < this.length; i++){
+       if (fn(this[i])) {
+         listCopy[i]=this[i];
+         listCopy.length++;
+       }
+     }
+
   return listCopy;
 }
