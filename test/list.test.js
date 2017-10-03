239e83636e203f93b95ae6ed768680a198cb7e15
meganr <meganrm@gmail.com>
2017-10-03 16:49:24 -0700

    added additional tests

--- test/list.test.js
+++ test/list.test.js
@@ -4,7 +4,8 @@ const List = require('../lib/list.js');
 const expect = require ('expect');
 
 describe('My New List', () => {
-  describe('structure', () => {
+
+  describe('list structure', () => {
     it('inits with a length of 0', () => {
       let list = new List();
       expect(list.length).toEqual(0);
@@ -19,13 +20,14 @@ describe('My New List', () => {
     it('increases the length of array by 1', () => {
       expect(list.length).toEqual(1);
     });
-    it('adds the new element to the end of the array', () => {
 
+    it('adds the new element to the end of the array', () => {
       expect(list[0]).toEqual(element);
     });
   });
 
   describe('pop() implemenation', () => {
+
     let list = new List();
     let element = 'foo';
     list.push(element);
@@ -34,46 +36,54 @@ describe('My New List', () => {
     it('decreases the length of array by 1', () => {
       expect(list.length).toEqual(0);
     });
+
     it('returns the last element from an array', () => {
       expect(popped).toEqual(element);
     });
+
     it('returns undefined if there isnt anything in the list', () => {
       let listNew = new List();
-      let noPop = list.pop();
+      let noPop = listNew.pop();
       expect(noPop).toEqual(undefined);
     });
   });
 
   describe('forEach() implemenation', () => {
     let list = new List();
-    list.push('foo')
-    list.push('bar')
-    let value = ''
-    let callback = (ele, index, array) => {
+    list.push('foo');
+    list.push('bar');
+    let value = '';
+    let callback = (ele) => {
       value = value + ele;
-    }
-    list.forEach(callback)
-    it('takes a callback and applies each element in a list', () => {
+    };
+    list.forEach(callback);
+
+    it('returns an error if callback is not a function', () => {
+      let notCallback = 'hello';
+      expect(list.forEach(notCallback).message).toBe('Callback must be a function');
+    });
+
+    it('takes a callback and applies it to each element in a list', () => {
       expect(value).toEqual('foobar');
     });
   });
 
   describe('map() implemenation', () => {
     let list = new List();
-    list.push('foo')
-    list.push('bar')
-    let callback = (ele, index, array) => {
+    list.push('foo');
+    list.push('bar');
+    let callback = (ele) => {
       return ele + '-dragon';
-    }
+    };
     let originalList = list;
-    let mapped = list.map(callback)
+    let mapped = list.map(callback);
 
     it('doesnt mutate the original list', () => {
       expect(originalList).toEqual(list);
     });
 
     it('takes a callback and applies each element and returns a list of those new elements', () => {
-      expect(mapped).toEqual({"0": "foo-dragon", "1": "bar-dragon", "length": 2});
+      expect(mapped).toEqual({'0': 'foo-dragon', '1': 'bar-dragon', 'length': 2});
     });
 
     it('returns a list with the same length as the original', () => {
@@ -83,38 +93,43 @@ describe('My New List', () => {
 
   describe('filter() implemenation', () => {
     let list = new List();
-    list.push('foo')
-    list.push('bar')
-    let callback = (ele, index, array) => {
+    list.push('foo');
+    list.push('bar');
+    let callback = (ele) => {
       return ele === 'foo';
-    }
-    let filtered = list.filter(callback)
+    };
+    let filtered = list.filter(callback);
+
+    it('returns an array of equal or less length', () => {
+      expect(filtered.length).toBeLessThanOrEqual(list.length);
+    });
+
     it('takes a callback, returns a new list with only elements that pass the callback test', () => {
-      expect(filtered).toEqual({"0": "foo", "length": 1});
+      expect(filtered).toEqual({'0': 'foo', 'length': 1});
     });
   });
 
   describe('reduce() implemenation', () => {
     let list = new List();
-    list.push('foo')
-    list.push('bar')
+    list.push('foo');
+    list.push('bar');
 
     it('can add up elements', () => {
-      let callback = (acc, cur, index, array) => {
-        return  acc + cur
-      }
-      let reduced = list.reduce(callback, 0)
+      let callback = (acc, cur) => {
+        return  acc + cur;
+      };
+      let reduced = list.reduce(callback, 0);
       expect(reduced).toEqual('0foobar');
     });
 
-    // it('can replicate the functionality of map()', () => {
-    //   let callback = (acc, cur, index, array) => {
-    //     acc.push(cur)
-    //     return acc
-    //   }
-    //   let reduced = list.reduce(callback)
-    //   expect(reduced).toEqual( {"0": "foo", "1": "bar", "length": 2});
-    // });
+    it('can replicate the functionality of map()', () => {
+      let callback = (acc, cur) => {
+        acc.push(cur);
+        return acc;
+      };
+      let reduced = list.reduce(callback);
+      expect(reduced).toEqual( {'0': 'foo', '1': 'bar', 'length': 2});
+    });
 
   });
 });
