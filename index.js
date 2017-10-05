0b58f720ad61813e007a37e12edbfc391c74e1f5
hanhthaoluu <hanhthaoluu@gmail.com>
2017-10-04 22:15:55 -0700

    implemented all functions, just need testing

---
+++ index.js
@@ -0,0 +1,64 @@
+'use strict';
+
+function List () {
+  //values are mutable data
+  //this = this instance
+  this.values = [];
+}
+
+//create an instance of List
+//let newList = new List();
+
+List.push = function(newItem) {
+  this.values.push(newItem);
+};
+
+
+List.pop = function() {
+//.pop removes the last element from the array. The returned value is the removed element from the array; returns 'undefined' if the array is empty.
+  return this.values.pop();
+};
+
+
+//pure functions only cares about their own inputs and spit out outputs.  They don't affect other things.
+List.forEach = function(callback) {
+  for(let i = 0; i < this.values.length; i++){
+    callback(this.values[i]);
+  }
+};
+
+
+List.filter = function(callback) {
+  let filtered = [];
+
+  for(let i = 0; i < this.values.length; i++){
+    if (callback(this.values[i]) === true) {
+      filtered.push(this.values[i]);
+    }
+  }
+
+  return filtered;
+};
+
+
+List.map = function(callback) {
+  let mapped = [];
+  for(let i = 0; i < this.values.length; i++){
+    let modifiedElement = callback(this.values[i]);
+    mapped.push(modifiedElement);
+  }
+
+  return mapped;
+};
+
+
+List.reduce = function(callback) {
+  let accumulator = this.values[0];
+
+  //i = index
+  for(let i = 1; i < this.values.length; i++){
+    accumulator = callback(accumulator, this.values(i), i, this.values);
+  }
+
+  return accumulator;
+};
